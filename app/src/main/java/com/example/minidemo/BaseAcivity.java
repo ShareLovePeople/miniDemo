cc5b2c4d4e05044259f384bb32a63bf60abd0cb8
1192293855 <1192293855@qq.com>
2019-04-17 14:12:53 +0800

    init

---
+++ app/src/main/java/com/example/minidemo/BaseAcivity.java
@@ -0,0 +1,13 @@
+package com.example.minidemo;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+
+/**
+ * @author Bob
+ * Date 2019/4/17
+ * Description
+ */
+@SuppressLint("Registered")
+public class BaseAcivity extends Activity {
+}
---
+++ app/src/main/java/com/example/minidemo/BrowserActivity.java
@@ -0,0 +1,16 @@
+package com.example.minidemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author Bob
+ */
+public class BrowserActivity extends BaseAcivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browser);
+    }
+}
